a2d7a369e97d05529c7cafa480c272e28797c40b
Franco1605 <ffcastillo@uc.cl>
2020-05-02 12:22:41 -0400

    segundo deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'hamburguesa/' => 'hamburgers#index'
+      get 'hamburguesa' => 'hamburgers#index'
       get 'hamburguesa/:id' => 'hamburgers#show' 
-      post 'hamburguesa/' => 'hamburgers#create'
+      post 'hamburguesa' => 'hamburgers#create'
       put 'hamburguesa/:id_hamburguesa/ingrediente/:id_ingrediente' => 'hamburgers#add_ingredient'
       patch 'hamburguesa/:id' => 'hamburgers#edit_hamburger'
       delete 'hamburguesa/:id' => 'hamburgers#destroy'
       delete 'hamburguesa/:id_hamburguesa/ingrediente/:id_ingrediente'=> 'hamburgers#delete_ingredient'
-      get 'ingrediente/' => 'ingredients#index'
+      get 'ingrediente' => 'ingredients#index'
       get 'ingrediente/:id' => 'ingredients#show' 
-      post 'ingrediente/' => 'ingredients#create'
+      post 'ingrediente' => 'ingredients#create'
       delete 'ingrediente/:id' => 'ingredients#destroy'
     end
   end
